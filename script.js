cb2c6d140dea409642866a4d804eec4ac406fde1
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-05-23 14:26:07 +0200

    Geting pakimon by name and id

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 
+document.getElementById("run").addEventListener("click", pokemonInfo);
 
-const api_url = "https://pokeapi.co/api/v2/pokemon/ditto"
-
-    async function getPokemon () {
-    let data = await fetch(api_url)
-        let main = await data.json()
-        console.log(main)
-    }
+async function pokemonInfo () {
+    const api_url = "https://pokeapi.co/api/v2/pokemon/";
+    let pokeInfo = document.getElementById("pokemon-id").value;
+    let input = pokeInfo;
 
 
-getPokemon()
 
+    let data = await fetch(`${api_url}${input}`)
+    let res = await data.json();
+    console.log(res)
+}
